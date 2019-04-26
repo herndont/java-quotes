32de6cc9379d60ff4b658756196ba42dbe6097fe
herndont <herndontrey@gmail.com>
2019-04-25 21:23:10 -0700

    latest update

--- src/test/java/quotes/AppTest.java
+++ src/test/java/quotes/AppTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AppTest {
-
+//I think we are going to need to break up our methods into smaller chunks to be able to test this properly.
     @Test
     public void main() {
     }
