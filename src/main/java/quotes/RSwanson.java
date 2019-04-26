159d828e40277cbdb3b800a089001366db6d42b7
herndont <herndontrey@gmail.com>
2019-04-26 09:12:55 -0700

    new branch for continued work. Pushing up for synching with team

---
+++ src/main/java/quotes/RSwanson.java
@@ -0,0 +1,14 @@
+package quotes;
+
+public class RSwanson {
+    public String text;
+
+    public RSwanson(String text) {
+        this.text = text;
+    }
+
+    public String toString() {
+        return "Famed theologian, Ron Swanson once said: " + text;
+    }
+}
+
